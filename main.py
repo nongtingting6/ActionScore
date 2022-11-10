5c0961c95e192ae9a281157995eed34aaaf99aaf
nongtingting6 <1574416650@qq.com>
2022-11-10 23:23:50 +0800

    简单的输入

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+a={'1':4,'2':5}
+
+ntt = 0
+liu=0
+while True:
+    name = input('请输入你的名字')
+    type = input("请输入类别")
+    score = a.get(type)
+    if name == 'liu':
+        liu +=score
+        print(f"刘双喜分数:{liu}")
+    elif name=='ntt':
+        ntt +=score
+        print("农婷婷分数:",ntt)
+
+
+
+
