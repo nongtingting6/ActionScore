5f09c6959cb9977610037d48e528f8c2685ea6ca
nongtingting6 <1574416650@qq.com>
2022-11-12 23:50:18 +0800

    1

--- person.py
+++ person.py
@@ -1,21 +1,19 @@
 import datetime
+from openpyxl import load_workbook
+
+
 class Person:
 
-    def __init__(self,score):
+    def __init__(self, score):
         self.score = score
-        self.action_and_time=[]
-
-
+        self.action_and_time = []
 
     def action(self, action_id):
-
         action_score = {'1': 4, '2': 5}
 
-        cur_score=action_score.get(action_id,0)
-        cur_day=str(datetime.datetime.now())
+        cur_score = action_score.get(action_id, 0)
+        cur_day = str(datetime.datetime.now())
         cur_time = cur_day.split(".")[0]
 
-        self.score=self.score+cur_score
-        self.action_and_time = self.action_and_time + [action_id,str(cur_time)]
-
-
+        self.score = self.score + cur_score
+        self.action_and_time = self.action_and_time + [action_id, str(cur_time)]
