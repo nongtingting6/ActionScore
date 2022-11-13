7c07d5f6c16c44ba7284e7e4bb63993d3e3a88b7
nongtingting6 <1574416650@qq.com>
2022-11-13 20:32:02 +0800

    1

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 from person import Person
+from data import Data
 import datetime
+from openpyxl import load_workbook
 
-ntt=Person(0)
-liu=Person(0)
+ntt = Person(0)
+liu = Person(0)
+obj_dict = {'liu': liu, 'ntt': ntt}
+
+
 while True:
-    name = input('请输入你的名字')
-    action_id = input("请输入动作id")
-    if name=="liu":
-        liu.action(action_id)
-    elif name=="ntt":
-        ntt.action(action_id)
+    _input = input('输入名字和动作id，例如：ntt 1')  # ntt 1
+    name = _input.split(' ')[0]
+    action_id = _input.split(' ')[1]
+    # 选择b本次需要操作的对象
+    obj = obj_dict.get(name)
 
 
-    print(f"刘双喜的分数为：{liu.score}")
-    print(f"农婷婷的分数为：{ntt.score}")
-    print(f'当前农婷婷做：{ntt.action_and_time}')
+    # 判断输入
+    # 输入是S，则保存
+    if action_id=='S':
+        date = load_workbook("data.xlsx")  # 加载
+        sheet = date["记录事件"]  # 选择编辑的工作簿
+        for record in obj.action_and_time:
+            sheet["A1"]=name+record[0]+record[1]
+        date.save('data.xlsx')
 
 
 
+    elif action_id=='Q':
+        exit('退出程序')
+
+    # 输入是Q，则推出
+    # 输入是其他，则运行动作
+    else:
+        name = _input.split(' ')[0]
+        action_id = _input.split(' ')[1]
+        # 选择b本次需要操作的对象
+        obj = obj_dict.get(name)
+        if obj is not None:
+            obj.action(action_id)
+
+    print(f"当前操作的对象{name}的分数为：{obj.score}")
+    print(f"当前操作的对象{name}的记录为：{obj.action_and_time}")
--- person.py
+++ person.py
@@ -1,19 +1,35 @@
 import datetime
 from openpyxl import load_workbook
+from data import Data
 
-
-class Person:
+class Person():
 
     def __init__(self, score):
         self.score = score
         self.action_and_time = []
 
     def action(self, action_id):
-        action_score = {'1': 4, '2': 5}
+        # 从excel获取所有动作的信息
+        date = Data("data.xlsx")
+        action_to_info = date.get_data()
 
-        cur_score = action_score.get(action_id, 0)
-        cur_day = str(datetime.datetime.now())
-        cur_time = cur_day.split(".")[0]
+        # 获取当前动作的信息，是列表[动作，范围，频率，分数]
+        action_info_cur = action_to_info.get(action_id)
 
+        # 如果当前动作id在excel的所有动作信息里的话
+        if action_info_cur is not None:
+            cur_score = action_info_cur[3]
+            cur_action_info = action_info_cur[0]
+        else:
+            cur_score = 0
+            cur_action_info = '输入错误'
+
+        # 获取当前时间的datetime对象
+        cur_day = datetime.datetime.now()
+        # datatime格式化输出
+        cur_time = cur_day.strftime('%Y年%m月%d日%H点%M分%S秒')
+
+        # 累加保存当前对象的分数和动作记录
         self.score = self.score + cur_score
-        self.action_and_time = self.action_and_time + [action_id, str(cur_time)]
+        self.action_and_time = self.action_and_time + [[cur_action_info, cur_time]]
+
